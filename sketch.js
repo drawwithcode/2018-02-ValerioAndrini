fb05bc17ed1237a1b29bd0f58e21d25a1708a548
Valerio Andrini <valerio.andrini@gmail.com>
2018-09-30 10:42:54 +0200

    new effect and update colors

--- sketch.js
+++ sketch.js
@@ -5,23 +5,57 @@ function preload(){
 function setup() {
   // put setup code here
   createCanvas(windowWidth,windowHeight);
-  background(200);
+  background(0);
   angleMode(DEGREES);
   frameRate(180);
 }
 
 function draw() {
   // put drawing code here
-  translate(width/2,height/2);
+
+  //angoli e trasformazioni
+  var angle = 180 * sin(frameCount * 4);
+  var angle2 = 200 * sin(frameCount);
+  var angle3 = 200 * cos(frameCount);
+
+  translate(width / 2, height / 2);
   rotate(frameCount);
-  stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/180));
-  line(200*cos(frameCount),0,0,200*sin(frameCount));
 
-  stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/180));
-  line(200*cos(frameCount),0,-200,-200*sin(frameCount));
+  //colori
+  stroke(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
 
-  stroke(lerpColor(color('#ea0043'), color('#0fefca'), frameCount/180));
-  line(200*cos(frameCount),0,200,-200*sin(frameCount));
+  //petali
+  line(0, 0, 0, angle);
+  line(0, 0, 0, -angle);
+
+  //colori
+  fill(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
+  noStroke();
+
+  //anello
+  ellipse(150, 150, 10, 10)
+  ellipse(150, -150, 10, 10)
+  ellipse(-150, 150, 10, 10)
+  ellipse(-150, -150, 10, 10)
+  
+  //pillola verticale
+  // push();
+  // rotate(-90);
+  // stroke(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
+  // line(angle3,0,0,angle2);
+  // pop();
+  // //pillola orizzontale
+  // push();
+  // stroke(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
+  // line(angle3,0,0,angle2);
+  // pop();
+
+  //EFFETTO CUORE
+  // rotate(-90);
+  // stroke(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
+  // line(angle3,0,-200,-angle2);
+  // stroke(lerpColor(color('#6666FF'), color('#FF6633'), frameCount / 210));
+  // line(angle3,0,200,-angle2);
 
 if (frameCount == 180) {
   noLoop();
